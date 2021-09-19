9715be7c011496397800be914285999d7d058b85
Pietro Iglio <igliop@gmail.com>
2021-09-19 19:34:34 +0200

    added SSR

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className="container">
       <Head>
@@ -18,33 +18,9 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {props.data.map(val => ( 
+            <div key={val.id}>{val.title}</div>
+          ))} 
         </div>
       </main>
 
@@ -206,4 +182,22 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/albums')
+  const data = await res.json()
+
+  if (!data) {
+      return {
+          notFound: true,
+      }
+  }
+  
+  return {
+    props: {
+      // props for your component, that will be passed to the page component as props
+      data: data
+    }
+  }
 }
